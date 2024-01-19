cdd8121ac0e476528e2159b08e0015d46189bb99
Chris Oates <chrispoates@gmail.com>
2024-01-19 15:06:10 -0500

    Added a listener to do stuff when changing tabs. Also started reformatting Scala files the settings I normally have.

--- src/main/scala/org/jetbrains/scala/samples/listeners/FileOpenedListener.scala
+++ src/main/scala/org/jetbrains/scala/samples/listeners/FileOpenedListener.scala
@@ -3,17 +3,43 @@
 package org.jetbrains.scala.samples.listeners
 
 import com.intellij.notification.{Notification, NotificationType, Notifications}
-import com.intellij.openapi.fileEditor.{FileEditorManager, FileEditorManagerListener}
+import com.intellij.openapi.fileEditor.{FileEditorManager, FileEditorManagerEvent, FileEditorManagerListener}
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.wm.{ToolWindow, ToolWindowManager}
 import org.jetbrains.scala.samples.SamplePluginBundle
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+import com.intellij.psi.{PsiDocumentManager, PsiFile}
+import com.intellij.ui.components.JBLabel
 
 class FileOpenedListener extends FileEditorManagerListener {
-  override def fileOpened(source: FileEditorManager, file: VirtualFile): Unit = {
-    Notifications.Bus.notify(
-      new Notification("My Plugin Notification",
-        SamplePluginBundle.message("file.opened"),
-        SamplePluginBundle.message("name.getname", file.getName),
-        NotificationType.INFORMATION)
-    )
-  }
+
+    val logger: Logger = Logger.getFactory.getLoggerInstance(getClass.getName)
+
+    override def fileOpened(source: FileEditorManager, file: VirtualFile): Unit = {
+        Notifications.Bus.notify(
+            new Notification("My Plugin Notification",
+                SamplePluginBundle.message("file.opened"),
+                SamplePluginBundle.message("name.getname", file.getName),
+                NotificationType.INFORMATION)
+        )
+        logger.info(s"Files: [${source.getOpenFiles.mkString(", ")}]")
+    }
+
+    override def selectionChanged(event: FileEditorManagerEvent): Unit = {
+        val oldFileName = Option(event.getOldFile).map(_.getName).getOrElse("<none>")
+        val newFileName = Option(event.getNewFile).map(_.getName).getOrElse("<none>")
+        logger.info(s"Selection changed, oldFile=$oldFileName, newFile=$newFileName")
+
+        // use for updating the tool window??
+        val toolWindow: ToolWindow = ToolWindowManager.getInstance(event.getManager.getProject).getToolWindow("ChrisFoolWindow")
+
+
+        val project: Project = event.getManager.getProject
+        val currentDoc = FileEditorManager.getInstance(project).getSelectedTextEditor().getDocument()
+        val psiFile: PsiFile = PsiDocumentManager.getInstance(project).getPsiFile(currentDoc)
+        val firstChildText = Option(psiFile.getFirstChild).map(_.getText).getOrElse("<none>")
+        logger.info(s"First child text: $firstChildText")
+
+    }
 }
--- src/main/scala/org/jetbrains/scala/samples/ui/ChrisToolWindowFactory.scala
+++ src/main/scala/org/jetbrains/scala/samples/ui/ChrisToolWindowFactory.scala
@@ -2,14 +2,32 @@
 
 package org.jetbrains.scala.samples.ui
 
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.{ToolWindow, ToolWindowFactory}
 import com.intellij.ui.components.JBLabel
 import org.jetbrains.scala.samples.SamplePluginBundle
+import com.intellij.openapi.diagnostic.Logger
 
 class ChrisToolWindowFactory extends ToolWindowFactory {
 
-  override def createToolWindowContent(project: Project, toolWindow: ToolWindow): Unit =
-    toolWindow.getComponent.add(new JBLabel(SamplePluginBundle.message("chris.tool.window")))
+    val logger: Logger = Logger.getFactory.getLoggerInstance(getClass.getName)
+
+    override def createToolWindowContent(project: Project, toolWindow: ToolWindow): Unit = {
+
+        val theText = for {
+            proj <- Option(project)
+            instance <- Option(FileEditorManager.getInstance(proj))
+            editor <- Option(instance.getSelectedTextEditor())
+            doc = editor.getDocument()
+            txt = doc.getText()
+        } yield txt
+
+        toolWindow.getComponent.add(new JBLabel(s"Hey ${theText.getOrElse("<empty>")}"))
+
+        logger.info("This is a message!!")
+
+        //toolWindow.getComponent.add(new JBLabel(SamplePluginBundle.message("chris.tool.window")))
+    }
 
 }
